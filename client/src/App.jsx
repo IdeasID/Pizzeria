e17d8b647ab1bab7ad054aabfe7eb1d498ad9147
Angot <juandiaries@hotmail.com>
2023-08-22 11:16:12 -0500

    login start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import NavbarInv from "./Header/NavbarInv";
 import { CardProducto } from "./components/cardEditProducto/CardProducto.jsx";
+
 function App() {
   return (
     <>
@@ -7,6 +8,7 @@ function App() {
       <CardProducto />
     </>
   );
+  
 }
 
 export default App;
